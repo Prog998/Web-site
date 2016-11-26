d64a24699c1d2f428a9c5a3458aeb5e4e5941d77
Prog998 <j.j.virus@hotmail.com>
2016-11-26 15:00:26 -0600

    Tarea6

---
+++ math.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+   "fmt"
+   "math"
+)
+
+func main() {
+   
+   fmt.Println("Feliz dia", math.Pi)
+}
---
+++ swap.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+   
+   return y, x
+}
+
+func main() {
+
+   a, b := swap("hola", "mundo")
+   fmt.Println(a, b)
+}
